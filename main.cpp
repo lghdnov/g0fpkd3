1e56635c1c1bfa3bdf337e459774447d7620376e
glitch-cat <lghdvanov@yandex.ru>
2024-03-06 18:34:41 +0300

    1.1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <fstream>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    ifstream file("a.txt");
+    int p_counter = 0, n_counter = 0, n;
+    while (file >> n){
+        if (n > 0) p_counter++;
+        if (n < 0) n_counter++;
+    }
+    printf("positive: %i, negative: %i", p_counter, n_counter);
+    file.close();
 }

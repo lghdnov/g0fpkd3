9e8289be74f9835d494c5b912d90f7965091c12c
glitch-cat <lghdvanov@yandex.ru>
2024-03-06 18:50:24 +0300

    2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <fstream>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    ifstream first_file("a1.txt"),
+            second_file("a2.txt");
+
+    float p_sum = 0, f = 0, s = 0;
+
+    while (first_file >> f){
+        if (f > 0) p_sum += f;
+    }
+    while (second_file >> s){
+        if (s > 0) p_sum += s;
+    }
+    cout << p_sum << "\n";
+    first_file.close();
+    second_file.close();
 }

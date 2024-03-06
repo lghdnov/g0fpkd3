6be50f35252e9c8cc4c8ea29fa30cd14e9fd8d34
glitch-cat <lghdvanov@yandex.ru>
2024-03-06 18:56:56 +0300

    3

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void print_file(string& filename) {
+    ifstream file(filename);
+
+    string line;
+    while (getline(file, line))
+        cout << line << endl;
+
+    file.close();
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    ifstream input_file("input.txt");
+    ofstream output_file("out.txt");
+
+    char ch;
+    while (input_file.get(ch))
+        output_file.put(toupper(ch));
+
+    input_file.close();
+    output_file.close();
+
+    string out_str = "out.txt";
+    print_file(out_str);
 }
